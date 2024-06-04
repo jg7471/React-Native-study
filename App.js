9ccd08f9e03c8653535bdbfbdeef58e4765d0bf9
jg <iamjgl@naver.com>
2024-06-04 19:40:32 +0900

    rns study

--- App.js
+++ App.js
@@ -1,85 +1,100 @@
-import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, FlatList, StyleSheet, View } from 'react-native';
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
+  const [modalIsVisible, setModalIsVisible] = useState(false);
   //입력창의 상태를 관리하는 변수를 React 에서 사용하는 useState 훅을 활용하여 선언
-  const [enteredGoalText, setEnteredGoalText] = useState('');
   const [todoGoals, setTodoGoals] = useState([]);
 
-  //사용자가 내용을 입력할 때 해당 입력값을 가져오는 함수
-  const goalInputHandler = (enteredText) => {
-    setEnteredGoalText(enteredText);
-  };
-
   //추가 버튼을 누르면 할 일 목록을 추가하는 함수
-  const addGoalHandler = () => {
+  const addGoalHandler = (enteredGoalText) => {
     console.log(enteredGoalText);
 
     //useState Setter 메서드의 스냡샷 방식
     //콜백 함수의 매개값은 해당 상태 변수의 최신 값이 전달됨
     setTodoGoals((currentTodoGoals) => [
-      //setTodoGoals([...todoGoals, enteredGoalText]) //요약
+      //setTodoGoals([...todoGoals, enteredGoalText]) //요약 @@@
       ...currentTodoGoals,
-      { text: enteredGoalText, key: Math.random().toString() }, //구분 해주기
-    ]); //@@@
+      { text: enteredGoalText, id: Math.random().toString() }, //구분 해주기
+    ]);
+
+    endAddGoalHandler();
   };
+
+  const deleteGoalHandler = (id) => {
+    setTodoGoals((currentTodoGoals) => {
+      //스냅샷방식 @@@
+      return currentTodoGoals.filter((goal) => goal.id !== id);
+    });
+  };
+
+  //할 일 추가 모달 띄어주는 함수
+  const startAddGoalhandler = () => {
+    //@@@ ()
+    setModalIsVisible(true);
+  };
+
+  const endAddGoalHandler = () => {
+    setModalIsVisible(false);
+  };
+
   return (
-    <View style={styles.container}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.textInput}
-          placeholder='할 일을 입력하세요!'
-          onChangeText={goalInputHandler}
+    <>
+      <StatusBar style='light' />
+      <View style={styles.appContainer}>
+        <Button
+          title='할 일 추가하기'
+          color='#5e0acc'
+          onPress={startAddGoalhandler}
         />
-        <Button title='할 일 추가하기' onPress={addGoalHandler} />
+        {/* modalIsVisible && 를 직접 구현할 필요 없음 @@@ */}
+        <GoalInput
+          visible={modalIsVisible}
+          onAddGoal={addGoalHandler}
+          onCancel={endAddGoalHandler}
+        />
+        <View style={styles.goalsContainer}>
+          {/* 
+          ScrollView는 전체화면 렌더링이 될 때 안의 항목들을 전부 렌더링 함.
+          이로 인해 성능 상의 저하가 나타날 수 있습니다.
+          (보이지 않는 영역까지 렌더링을 진행하기 때문에 목록이 많다면 로딩이 길어짐.
+ 
+            FlatList는 보이는 영역만 일단 렌더링을 진행하고, 나머지 항목들은
+            스크롤 움직이 발생하면 그 때 그 때 렌덩링을 진행함.
+            )
+        */}
+          {/* <ScrollView> 대체 */}
+          <FlatList
+            data={todoGoals}
+            renderItem={(itemData) => {
+              return (
+                <GoalItem
+                  text={itemData.item.text}
+                  id={itemData.item.id}
+                  onDeleteItem={deleteGoalHandler}
+                />
+              );
+            }}
+            keyExtractor={(item, index) => {
+              return item.id;
+            }}
+          ></FlatList>
+        </View>
       </View>
-      <View style={styles.goalsContainer}>
-        {todoGoals.map((goal) => (
-          <View key={goal.key} style={styles.goalItem}>
-            {/* view에 직접 적용 불가 */}
-            <Text style={styles.goalText}>{goal.text}</Text>
-          </View>
-        ))}
-        <Text>할 일 목록들...</Text>
-      </View>
-    </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    padding: 50,
+    paddingTop: 50,
     paddingHorizontal: 16,
-  },
-  inputContainer: {
-    flex: 1,
-    flexDirection: 'row', //기본값 column
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: '#cccccc',
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: '#cccccc',
-    width: '70%',
-    marginRight: 8,
-    padding: 8,
   },
   goalsContainer: {
     flex: 4,
-  },
-  goalItem: {
-    margin: 8,
-    padding: 8,
-    borderRadius: 6,
-    backgroundColor: '#5e0acc',
-    color: 'white',
-  },
-  goalText: {
-    color: 'white',
   },
 });
